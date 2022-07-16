f72a562fd7c7c0fa00c18069d4608441a0c8963e
Bart <bartosz.art@gmail.com>
2022-07-17 00:06:36 +0200

    first commit

---
+++ bujnicka-dent-mobile/bujnicka-dent-mobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bujnicka_dent_mobile
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
